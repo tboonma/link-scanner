f3900a873f34e55adf9e51ddb01adc115c34c760
tboonma <tboonma@hotmail.com>
2021-11-28 10:44:48 +0700

    Fix bugs in getting bad links and improve careful programming

--- link_scan.py
+++ link_scan.py
@@ -1,8 +1,8 @@
 import os
 import sys
-
+from urllib.request import urlopen
 import requests
-from requests.exceptions import ConnectionError
+from urllib.error import HTTPError
 from selenium import webdriver
 from selenium.webdriver.chrome.service import Service
 from selenium.webdriver.firefox.options import Options
@@ -25,12 +25,21 @@ def get_links(url: str) -> list[str]:
     browser = webdriver.Firefox(service=service, options=options)
     browser.get(url)
     all_a_tags = browser.find_elements("tag name", "a")
-    obtained_urls = []
+    obtained_urls = set()
     for tag in all_a_tags:
-        href_link = tag.get_attribute("href").split("#")
-        obtained_urls.append(href_link[0])
+        href_link = tag.get_attribute("href")
+        # check if there's a link in href
+        if not isinstance(href_link, str):
+            continue
+        href_link = href_link.split("?")  # remove parameters from link
+        href_link = href_link[0].split("#")  # remove page fragments
+        href_link = href_link[0]  # get link after sliced
+        # check if it's not None and not empty string
+        if len(href_link) < 4 and not href_link.startswith("http"):
+            continue
+        obtained_urls.add(href_link)
     browser.quit()
-    return obtained_urls
+    return list(obtained_urls)
 
 
 def is_valid_url(url: str) -> bool:
@@ -44,9 +53,10 @@ def is_valid_url(url: str) -> bool:
         Also return False is the URL has invalid syntax.
     """
     try:
-        requests.head(url)
+        with urlopen(url) as conn:
+            conn.close()
         return True
-    except ConnectionError:
+    except HTTPError:
         return False
 
 
@@ -67,17 +77,37 @@ def invalid_urls(urllist: list[str]) -> list[str]:
     return invalid
 
 
+def is_html_page(url: str) -> bool:
+    """Check whether URL is the html page or not.
+
+    Args:
+        url: url to check
+
+    Returns:
+        True if url is html, otherwise return false.
+    """
+
+    response = requests.head(url)
+    return "text/html" in response.headers["content-type"]
+
+
 if __name__ == '__main__':
     """Running from command line."""
     args_amount = len(sys.argv)
-    if args_amount != 2 or not is_valid_url(sys.argv[1]):
+    url = sys.argv[1]
+    if args_amount != 2 or not is_valid_url(url):
         print("Usage:  python3 link_scan.py url")
         sys.exit()
-    all_links = get_links(sys.argv[1])
-    bad_links = invalid_urls(all_links)
+    if not is_html_page(url):
+        print("URL is not HTML")
+        sys.exit()
+    all_links = get_links(url)
     print()
     for link in all_links:
         print(link)
+
+    # get bad link
+    bad_links = invalid_urls(all_links)
     if len(bad_links) > 0:
         print("\nBad Links:")
         for link in bad_links:
